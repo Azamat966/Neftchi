e4f518bcfd49e71d38981e6802d7aa56f74ebfd6
Tairbekova <asanovan766@gmail.com>
2024-04-26 20:08:30 +0300

    I

--- src/main/java/com/example/neftchi/api/admin/AddEmployeeAdminApi.java
+++ src/main/java/com/example/neftchi/api/admin/AddEmployeeAdminApi.java
@@ -1,12 +1,32 @@
 package com.example.neftchi.api.admin;
+import com.example.neftchi.dto.response.AboutResponse;
+import com.example.neftchi.dto.response.EmployeResponse;
+import com.example.neftchi.model.AddEmployee;
+import com.example.neftchi.model.enums.Language;
+import com.example.neftchi.service.AboutCompanyService;
+import com.example.neftchi.service.AddEmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/add/employee/admin")
 @PreAuthorize("hasAnyAuthority('ADMIN')")
 public class AddEmployeeAdminApi {
+    private final AddEmployeeService addEmployeeService;
+    @PostMapping("/admin/save/")
+    public EmployeResponse updateDescription(@RequestParam String  name_lastname,
+                                             @RequestParam  String position,
+                                             @RequestParam Language language,
+                                             @RequestParam String description) {
+        return addEmployeeService.save(name_lastname, position, language, description);
+    }
+    @DeleteMapping("/save/delete/{id}")
+    public String deleteById(@PathVariable Long id) {
+        addEmployeeService.deleteById(id);
+        return "deleted:" + id;
+    }
 }
--- src/main/java/com/example/neftchi/service/AddEmployeeService.java
+++ src/main/java/com/example/neftchi/service/AddEmployeeService.java
@@ -1,9 +1,44 @@
 package com.example.neftchi.service;
 
+import com.example.neftchi.dto.response.EmployeResponse;
+import com.example.neftchi.dto.response.MiniPartnersRespons;
+import com.example.neftchi.model.AddEmployee;
+import com.example.neftchi.model.MiniPartners;
+import com.example.neftchi.model.enums.Language;
+import com.example.neftchi.repository.AddEmployeeRepository;
+import com.example.neftchi.repository.MiniPartnersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AddEmployeeService {
+
+    private final AddEmployeeRepository addEmployeeRepository;
+
+
+    public EmployeResponse save(String name_lastname,
+                                String position, Language language, String description) {
+        AddEmployee addEmployee = new AddEmployee();
+        addEmployee.setName_lastname(name_lastname);
+        addEmployee.setPosition(position);
+        addEmployee.setLanguage(language);
+        addEmployee.setDescription(description);
+        addEmployee.setLocalDateTime(LocalDateTime.now());
+        addEmployeeRepository.save(addEmployee);
+        return EmployeResponse.builder()
+                .id(addEmployee.getId())
+                .name_lastname(addEmployee.getName_lastname())
+                .position(addEmployee.getPosition())
+                .language(addEmployee.getLanguage())
+                .description(addEmployee.getDescription())
+                .build();
+    }
+
+    public void deleteById(Long id) {
+        addEmployeeRepository.deleteById(id);
+    }
 }
