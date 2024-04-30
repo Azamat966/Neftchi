b0594d76966b6ea05047f32d706ec476168e0910
Madina-88 <ergasevamalika066.gmail.com>
2024-04-30 18:17:30 +0300

    k

--- src/main/java/com/example/neftchi/api/admin/RegulationsAdminApi.java
+++ src/main/java/com/example/neftchi/api/admin/RegulationsAdminApi.java
@@ -1,12 +1,38 @@
 package com.example.neftchi.api.admin;
+import com.example.neftchi.dto.response.RegulationsResponce;
+import com.example.neftchi.service.RegulationsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/regulations/admin")
 @PreAuthorize("hasAnyAuthority('ADMIN')")
 public class RegulationsAdminApi {
+    private final RegulationsService service;
+
+    @PostMapping("save")
+    public RegulationsResponce save(@RequestParam String regulations,
+                                    @RequestParam String file_pdf){
+        return service.save(regulations,file_pdf);
+    }
+    @PutMapping("update")
+    public RegulationsResponce update(
+            @RequestParam Long id,
+            @RequestParam String regulations,
+            @RequestParam String file_pdf
+    ){
+        return service.update(id,regulations,file_pdf);
+    }
+    @DeleteMapping("delete")
+    public void deleteById(@RequestParam Long id){
+        service.delete(id);
+    }
+    @GetMapping("find/all")
+    public List<RegulationsResponce> findAll(){
+        return service.findAll();
+    }
 }
--- src/main/java/com/example/neftchi/service/RegulationsService.java
+++ src/main/java/com/example/neftchi/service/RegulationsService.java
@@ -1,9 +1,57 @@
 package com.example.neftchi.service;
 
+import com.example.neftchi.dto.response.RegulationsResponce;
+import com.example.neftchi.model.Regulations;
+import com.example.neftchi.repository.RegulationsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RegulationsService {
+    private final RegulationsRepository regulationsRepository;
+
+    public RegulationsResponce save(String regulations,
+                                    String file_pdf){
+        Regulations regulations1 = new Regulations();
+        regulations1.setRegulations(regulations);
+        regulations1.setFile_pdf(file_pdf);
+        regulationsRepository.save(regulations1);
+        return RegulationsResponce.builder()
+                .id(regulations1.getId())
+                .regulations(regulations1.getRegulations())
+                .file_pdf(regulations1.getFile_pdf())
+                .build();
+    }
+    public RegulationsResponce update(Long id,
+                                      String regulations,
+                                      String file_pdf){
+        Regulations regulations1 = regulationsRepository.findById(id).orElseThrow(() -> new RuntimeException("Partner not found with ID: " + id));
+        regulations1.setRegulations(regulations);
+        regulations1.setFile_pdf(file_pdf);
+        regulationsRepository.save(regulations1);
+        return RegulationsResponce.builder()
+                .id(regulations1.getId())
+                .regulations(regulations1.getRegulations())
+                .file_pdf(regulations1.getFile_pdf())
+                .build();
+    }
+    public void delete(Long id){
+        Regulations regulations = regulationsRepository.findById(id).orElseThrow(() -> new RuntimeException("Partner not found with ID: " + id));
+        regulationsRepository.delete(regulations);
+    }
+    public List<RegulationsResponce> findAll(){
+        List<RegulationsResponce> regulationsResponces = new ArrayList<>();
+        for (Regulations regulations : regulationsRepository.findAll()){
+            RegulationsResponce regulationsResponce = new RegulationsResponce();
+            regulationsResponce.setRegulations(regulations.getRegulations());
+            regulationsResponce.setFile_pdf(regulations.getFile_pdf());
+            regulationsResponce.setId(regulations.getId());
+            regulationsResponces.add(regulationsResponce);
+        }
+        return regulationsResponces;
+    }
 }
